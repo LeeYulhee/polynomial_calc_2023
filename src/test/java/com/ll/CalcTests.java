dfce0e7ec6a26a7009a84783ff691319af8d13c1
LeeYulhee <reasonoflife39@gmail.com>
2023-03-16 17:07:33 +0900

    재귀함수 호출 구조 파악을 위한 디버그 기능

--- src/test/java/com/ll/CalcTests.java
+++ src/test/java/com/ll/CalcTests.java
@@ -11,41 +11,49 @@ public class CalcTests {
     void t1() {
         assertThat(Calc.run("1 + 1")).isEqualTo(2);
     }
+
     @Test
     @DisplayName("2 + 1 == 3")
     void t2() {
         assertThat(Calc.run("2 + 1")).isEqualTo(3);
     }
+
     @Test
     @DisplayName("2 + 2 == 4")
     void t3() {
         assertThat(Calc.run("2 + 2")).isEqualTo(4);
     }
+
     @Test
     @DisplayName("1000 + 280 == 1280")
     void t4() {
         assertThat(Calc.run("1000 + 280")).isEqualTo(1280);
     }
+
     @Test
     @DisplayName("2 - 1 == 1")
     void t5() {
         assertThat(Calc.run("2 - 1")).isEqualTo(1);
     }
+
     @Test
     @DisplayName("3 - 1 == 2")
     void t6() {
         assertThat(Calc.run("3 - 1")).isEqualTo(2);
     }
+
     @Test
     @DisplayName("100 - 20 == 80")
     void t7() {
         assertThat(Calc.run("100 - 20")).isEqualTo(80);
     }
+
     @Test
     @DisplayName("10 + 20 + 30 == 60")
     void t8() {
         assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
     }
+
     @Test
     @DisplayName("10 - 20 + 30 == 20")
     void t9() {
@@ -128,5 +136,23 @@ public class CalcTests {
     @DisplayName("((20 + 20)) + 20 == 60")
     void t22() {
         assertThat(Calc.run("((20 + 20)) + 20")).isEqualTo(60);
+    }
+
+    @Test
+    @DisplayName("(10 + 20) * 3 == 90")
+    void t23() {
+        assertThat(Calc.run("(10 + 20) * 3")).isEqualTo(90);
+    }
+
+    @Test
+    @DisplayName("10 + (10 + 5) == 25")
+    void t24() {
+        assertThat(Calc.run("10 + (10 + 5)")).isEqualTo(25);
+    }
+
+    @Test
+    @DisplayName("-(10 + 5) == -15")
+    void t25() {
+        assertThat(Calc.run("-(10 + 5)")).isEqualTo(-15);
     }
 }
